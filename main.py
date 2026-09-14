68673a440d8af633aff25ea65187711a40c71bec
fee-licks <sas.v.21.42@gmail.com>
2026-09-14 15:40:09 +0200

    4. implemented player into the loop

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0.0 #Setting up delta time
     while True:
         log_state()
@@ -13,8 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen)
         dt = clock.tick(60) / 1000
+        pygame.display.flip() # Keep this at the end of the logic loop
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

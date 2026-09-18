12fcc70635640e35409648f7bb7abb10677bf9d9
fee-licks <sas.v.21.42@gmail.com>
2026-09-18 20:22:13 +0200

    fixed shots not being logged properly

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     dt = 0.0 #Setting up delta time
